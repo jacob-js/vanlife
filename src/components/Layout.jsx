36b8607954937a41fbb795ffc45079e39a59dd05
Jacob Dev <mercihabam@gmail.com>
2023-04-05 11:28:58 +0200

    feat: add footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
 
 function Layout() {
   return (
-    <>
+    <div className="site-wrapper">
         <Header />
-        <Outlet />
-    </>
+        <main>
+            <Outlet />
+        </main>
+        <Footer />
+    </div>
   )
 }
 
