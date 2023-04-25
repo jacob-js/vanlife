3d9ab06a54a8c468a8cd90ed9bc0954e0064cf4b
Jacob Dev <mercihabam@gmail.com>
2023-04-25 18:29:46 +0200

    feat: handle form submition states

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,11 +9,20 @@ export function loader({request}) {
 export default function Login() {
     const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
     const message = useLoaderData();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     async function handleSubmit(e) {
         e.preventDefault();
-        const data = await loginUser(loginFormData);
-        console.log(data);
+        setError(null)
+        setLoading(true)
+        try {
+            const data = await loginUser(loginFormData);
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading(false)
+        }
     }
 
     function handleChange(e) {
@@ -27,7 +36,7 @@ export default function Login() {
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
-            {message && <h3 className="red">{message}</h3>}
+            {(error || message) && <h3 className="red">{error?.message || message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -43,7 +52,7 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button disabled={loading}>{loading ? "Submitting...": "Log in"}</button>
             </form>
         </div>
     )
